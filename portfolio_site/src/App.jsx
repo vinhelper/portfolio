20b7db35ff492dfba6ff733e6c88594a493715a0
vinhelper <nadal.arvin122@gmail.com>
2024-07-23 15:37:52 +0800

    FEAT: Add other new features

--- portfolio_site/src/App.jsx
+++ portfolio_site/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Work from "./components/Work";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div id="content" className="lg:pl-[240px]" onClick={closeMenu}>
+        <Home />
+        <Services />
+        <Work />
+        <About />
+        <Contact />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// Component.propTypes = {
+//   anyProp: PropTypes.any,
+//   booleanProp: PropTypes.bool,
+//   numberProp: PropTypes.number,
+//   stringProp: PropTypes.string,
+//   functionProp: PropTypes.func,
+//   arrayProp: PropTypes.array,
+//   objectPerop: PropTypes.object,
+//   symbolProp: PropTypes.symbol,
+// }
---
+++ portfolio_site/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        cardShadow: "-4px 4px 0 0 rgb(26, 188, 156)",
+        cardShadow2: "-6px 6px 0 0 rgb(26, 188, 156)",
+      },
+      backgroundImage: {
+        "side-bar":
+          "linear-gradient(rgba(52, 73, 94, 0.9), rgba(52, 73, 94, 1)), url(../images/sidebar-img.jpg)",
+      },
+      fontFamily: {
+        lato: ["Quicksand", "sans-serif"],
+      },
+      colors: {
+        body: "#d4e6f1",
+        heading: "#f7f9f9",
+        base: "#34495e",
+        base2: "#2c3e50",
+        brand: "#1abc9c",
+        brand2: "#16a085",
+      },
+      // colors: {
+      //   body: "#f5f5f5", // Very light grey
+      //   heading: "#424242", // Dark grey
+      //   base: "#212121", // Almost black
+      //   base2: "#1b1b1b", // Darker almost black
+      //   brand: "#ffab40", // Light orange
+      //   brand2: "#ff9100", // Bright orange
+      // },
+    },
+  },
+  plugins: [],
+};
